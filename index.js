73e30c01350dc5f98b2dd4e44f78d0df633dba1e
Sergey Gavrilov <ses3332002@gmail.com>
2021-04-24 10:55:15 +0300

    fresh code

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import {
   url,
 } from './variables.js';
 
+import styles from './style';
 const DOMFrameBlocks = [];
 
 function renderSemanticBlock(block) {
@@ -23,7 +24,7 @@ function renderFooter() {
 
 function getTrackingData(event) {
   event.preventDefault();
-  const requests = Array.from(document.querySelectorAll('.request_set'));
+  const requests = Array.from(document.querySelectorAll(`.${styles.request_set}`));
   const requestDocuments = [];
   requests.forEach(prepareDocument);
 
@@ -43,7 +44,6 @@ function getTrackingData(event) {
   };
   requestTemplate.methodProperties.Documents = requestDocuments;
   requestOptions.body = JSON.stringify(requestTemplate);
-
   fetch(url, requestOptions)
     .then(response => {
       return response.json();
@@ -54,28 +54,30 @@ function getTrackingData(event) {
 }
 
 function renderResult(resultData) {
-  const trackingResultFields = Array.from(document.querySelectorAll('.tracking_result'));
+  const trackingResultFields = Array.from(document.querySelectorAll(`.${styles.tracking_result}`));
   resultData.forEach((item, index) => {
     trackingResultFields[index].innerHTML = `<h2>${topLevelStrings.resultString}:</h2>
-  <div class="tracking_result__wrapper">
-    <div class="tracking-result__caption">${UIStrings.routeString}:</div>
-    <div class="tracking-result__text tracking-result__text-route">${item.CitySender}-${
-      item.CityRecipient
-    }</div>
-    <div class="tracking-result__caption">${UIStrings.weightString}:</div>
-    <div class="tracking-result__text tracking-result__text-weight">${item.DocumentWeight}</div>
-    <div class="tracking-result__caption">${UIStrings.placesString}:</div>
-    <div class="tracking-result__text tracking-result__text-weight">${item.SeatsAmount}</div>
-    <div class="tracking-result__caption">${UIStrings.statusString}:</div>
-    <div class="tracking-result__text tracking-result__text-status">${item.Status}</div>
-    <div class="tracking-result__caption">${UIStrings.returnString}:</div>
-    <div class="tracking-result__text tracking-result__text-status">${
+  <div class=${styles.tracking_result__wrapper}>
+    <div class=${styles.tracking_result__caption}>${UIStrings.routeString}:</div>
+    <div class=${styles.tracking_result__text}>${item.CitySender}-${item.CityRecipient}</div>
+    <div class=${styles.tracking_result__caption}>${UIStrings.weightString}:</div>
+    <div class=${styles.tracking_result__text}>${item.DocumentWeight}</div>
+    <div class=${styles.tracking_result__caption}>${UIStrings.placesString}:</div>
+    <div class=${styles.tracking_result__text}>${item.SeatsAmount}</div>
+    <div class=${styles.tracking_result__caption}>${UIStrings.statusString}:</div>
+    <div class=${styles.tracking_result__text}>${item.Status}</div>
+    <div class=${styles.tracking_result__caption}>${UIStrings.returnString}:</div>
+    <div class=${styles.tracking_result__text}>${
       item.PossibilityCreateReturn ? UIStrings.yesString : UIStrings.noString
-    }<input type="button" class="request_action" disabled value="${UIStrings.requestString}"/></div>
-    <div class="tracking-result__caption">${UIStrings.redirectString}:</div>
-    <div class="tracking-result__text tracking-result__text-status">${
+    }<input type="button" class=${styles.request_action} disabled value="${
+      UIStrings.requestString
+    }"/></div>
+    <div class=${styles.tracking_result__caption}>${UIStrings.redirectString}:</div>
+    <div class="${styles.tracking_result__text} ${styles.tracking_result__text - status}">${
       item.PossibilityCreateRedirecting ? UIStrings.yesString : UIStrings.noString
-    }<input type="button" class="request_action" disabled value="${UIStrings.requestString}"/></div>
+    }<input type="button" class=${styles.request_action} disabled value="${
+      UIStrings.requestString
+    }"/></div>
   </div>`;
   });
 }
@@ -86,10 +88,10 @@ window.removeRequestSet = removeRequestSet;
 
 function renderMain() {
   DOMFrameBlocks[1].innerHTML = `
-    <div class="tracking_form">
+       <div class=${styles.tracking_form}>
       <form name="request_form" autocomplete="off" onsubmit="getTrackingData(event)">
         ${renderRequestSet()}
-        <input type="submit" value="${UIStrings.buttonString}" class="request_submit" />
+        <input type="submit" value="${UIStrings.buttonString}" class=${styles.request_submit} />
       </form>
     </div>`;
 }
@@ -104,18 +106,18 @@ function renderDynamicBlocks() {
 }
 
 function renderRequestSet() {
-  return `<div class="request_set">
-            <div class="request_item">
+  return `<div class=${styles.request_set}>
+            <div class=${styles.request_item}>
               <input type="text" name="invoice_number" value="" placeholder="" required/>
               <label for="invoice_number">${UIStrings.invoiceString}</label>
             </div>
-            <div class="request_item">
+            <div class=${styles.request_item}>
               <input type="text" name="phone_number" value="" pattern="[0-9]{0,10}" placeholder=""/>
               <label for="phone_number">${UIStrings.phoneString}</label>
             </div>
-            <input type="button" class="request_add_set" onclick="addRequestSet(event)" value="+"/>
-            <input type="button" class="request_remove_set" onclick="removeRequestSet(event)" value="-"/>
-             <div class="tracking_result"></div>
+            <input type="button" class=${styles.request_add_set} onclick="addRequestSet(event)" value="+"/>
+            <input type="button" class=${styles.request_remove_set} onclick="removeRequestSet(event)" value="-"/>
+             <div class=${styles.tracking_result}></div>
           </div>`;
 }
 
