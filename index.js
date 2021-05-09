cd66071c467764347e04c6f99e336a6c8d6df1fa
Sergey Gavrilov <ses3332002@gmail.com>
2021-05-10 00:15:15 +0300

    minor style fixes, code refactoring, multilingualism added, version 1.4

--- index.js
+++ index.js
@@ -1,46 +1,70 @@
-import { topLevelFrameBlocks, UIStrings, requestOptions, url, freshnessTime } from './variables.js';
+import {
+  topLevelFrameBlocks,
+  UIStrings,
+  requestOptions,
+  url,
+  freshnessTime,
+  appVersion,
+} from './variables.js';
 
 import styles from './style';
-const DOMFrameBlocks = [];
 
 // console.log(process.env.API_KEY);
+
+window.appState = {
+  lang: 'ru',
+  documentsForRender: [],
+  requestedDocuments: [],
+};
 
 function renderSemanticBlock(block) {
   return `<${block}></${block}>`;
 }
 
-function renderHeader() {
-  DOMFrameBlocks[0].innerHTML = `<h1>${UIStrings.headerString}</h1>`;
+function renderHeader(lang = window.appState.lang) {
+  document.querySelector('header').innerHTML = `<h1>${UIStrings[lang].headerString}</h1>
+  <div class=${styles.lang_choose}>
+    <button class=${styles.lang_choose__btn} data-lang="ru" onclick="changeLanguage(event)"></button>
+    <button class=${styles.lang_choose__btn} data-lang="ua" onclick="changeLanguage(event)"></button>
+  </div>`;
 }
 
-function renderFooter() {
-  DOMFrameBlocks[DOMFrameBlocks.length - 1].innerHTML = `${UIStrings.footerString}`;
-}
-
-function getTrackingData(event) {
-  event.preventDefault();
-  const requestSets = Array.from(document.querySelectorAll(`.${styles.request_set}`));
-  const requestedDocuments = [];
-  requestSets.forEach(prepareDocuments);
-
-  function prepareDocuments(requestItem) {
-    requestedDocuments.push({
-      DocumentNumber: requestItem.querySelector('input[name = invoice_number]').value,
-      Phone: requestItem.querySelector('input[name = phone_number]').value,
-    });
+function changeLanguage(event, lang = window.appState.lang) {
+  if (lang == event.target.dataset.lang) {
+    return;
+  } else {
+    window.appState.lang = event.target.dataset.lang;
+    renderApp();
+    document
+      .querySelector(`.${styles.lang_choose__btn}[data-lang=${window.appState.lang}]`)
+      .focus();
   }
+}
+
+function renderFooter(lang = window.appState.lang) {
+  document.querySelector('footer').innerHTML = `${UIStrings[lang].footerString} ${appVersion}`;
+}
+
+function getTrackingData(
+  event,
+  lang = window.appState.lang,
+  documentsForRender = window.appState.documentsForRender,
+  requestedDocuments = window.appState.requestedDocuments,
+) {
+  event.preventDefault();
+  documentsForRender.length = 0;
 
   const requestTemplate = {
     modelName: 'TrackingDocument',
     calledMethod: 'getStatusDocuments',
     methodProperties: {
-      Language: 'RU',
+      Language: lang,
     },
   };
 
-  const documentsForRender = [];
   const documentsForDownload = [];
   requestedDocuments.forEach(checkLocalStorage);
+
   function checkLocalStorage(requestedItem, index) {
     if (
       localStorage.getItem(requestedItem.DocumentNumber) &&
@@ -55,7 +79,7 @@ function getTrackingData(event) {
   }
 
   if (documentsForDownload.length == 0) {
-    renderResult(documentsForRender);
+    renderResults(documentsForRender);
   } else {
     requestTemplate.methodProperties.Documents = documentsForDownload;
     requestOptions.body = JSON.stringify(requestTemplate);
@@ -65,103 +89,138 @@ function getTrackingData(event) {
         return response.json();
       })
       .then(result => {
-        manageResults(result.data);
+        manageResults(result.data, documentsForRender);
       })
       .catch(err => {
         console.error('there was some error:', err);
       });
   }
+}
 
-  function manageResults(downloadedResults) {
-    documentsForRender.forEach((result, index) => {
-      if (result == 'gotToBeDownloaded') {
-        result = downloadedResults.shift();
-        result.requestTime = +new Date();
-        localStorage.setItem(result.Number, JSON.stringify(result));
-        documentsForRender[index] = result;
-      }
-    });
-    renderResult(documentsForRender);
+// storing downloaded data to local storage and preparing data for render results
+function manageResults(downloadedResults, documentsForRender) {
+  documentsForRender.forEach((result, index) => {
+    if (result == 'gotToBeDownloaded') {
+      result = downloadedResults.shift();
+      result.requestTime = +new Date();
+      localStorage.setItem(result.Number, JSON.stringify(result));
+      documentsForRender[index] = result;
+    }
+  });
+  renderResults(documentsForRender);
+}
+
+function renderResults(
+  resultsData = window.appState.documentsForRender,
+  lang = window.appState.lang,
+) {
+  const trackingResultFields = Array.from(document.querySelectorAll(`.${styles.tracking_result}`));
+  resultsData.forEach((item, index) => {
+    trackingResultFields[index].innerHTML = prepareResultString(item);
+  });
+
+  function prepareResultString(resultItem) {
+    return `<h2>${UIStrings[lang].resultString}:</h2>
+  <div class=${styles.tracking_result__wrapper}>
+    <div class=${styles.tracking_result__caption}>${UIStrings[lang].routeString}:</div>
+    <div class=${styles.tracking_result__text}>${resultItem.CitySender}-${
+      resultItem.CityRecipient
+    }</div>
+    <div class=${styles.tracking_result__caption}>${UIStrings[lang].weightString}:</div>
+    <div class=${styles.tracking_result__text}>${resultItem.DocumentWeight}</div>
+    <div class=${styles.tracking_result__caption}>${UIStrings[lang].placesString}:</div>
+    <div class=${styles.tracking_result__text}>${resultItem.SeatsAmount}</div>
+    <div class=${styles.tracking_result__caption}>${UIStrings[lang].statusString}:</div>
+    <div class=${styles.tracking_result__text}>${resultItem.Status}</div>
+    <div class=${styles.tracking_result__caption}>${UIStrings[lang].returnString}:</div>
+    <div class=${styles.tracking_result__text}>${
+      resultItem.PossibilityCreateReturn ? UIStrings[lang].yesString : UIStrings[lang].noString
+    }<input type="button" class=${styles.request_action} disabled value="${
+      UIStrings[lang].requestString
+    }"/></div>
+    <div class=${styles.tracking_result__caption}>${UIStrings[lang].redirectString}:</div>
+    <div class=${styles.tracking_result__text}>${
+      resultItem.PossibilityCreateRedirecting ? UIStrings[lang].yesString : UIStrings[lang].noString
+    }<input type="button" class=${styles.request_action} disabled value="${
+      UIStrings[lang].requestString
+    }"/></div>
+  </div>`;
   }
 }
 
-function renderResult(resultsData) {
-  const trackingResultFields = Array.from(document.querySelectorAll(`.${styles.tracking_result}`));
-  resultsData.forEach((item, index) => {
-    trackingResultFields[index].innerHTML = `<h2>${UIStrings.resultString}:</h2>
-  <div class=${styles.tracking_result__wrapper}>
-    <div class=${styles.tracking_result__caption}>${UIStrings.routeString}:</div>
-    <div class=${styles.tracking_result__text}>${item.CitySender}-${item.CityRecipient}</div>
-    <div class=${styles.tracking_result__caption}>${UIStrings.weightString}:</div>
-    <div class=${styles.tracking_result__text}>${item.DocumentWeight}</div>
-    <div class=${styles.tracking_result__caption}>${UIStrings.placesString}:</div>
-    <div class=${styles.tracking_result__text}>${item.SeatsAmount}</div>
-    <div class=${styles.tracking_result__caption}>${UIStrings.statusString}:</div>
-    <div class=${styles.tracking_result__text}>${item.Status}</div>
-    <div class=${styles.tracking_result__caption}>${UIStrings.returnString}:</div>
-    <div class=${styles.tracking_result__text}>${
-      item.PossibilityCreateReturn ? UIStrings.yesString : UIStrings.noString
-    }<input type="button" class=${styles.request_action} disabled value="${
-      UIStrings.requestString
-    }"/></div>
-    <div class=${styles.tracking_result__caption}>${UIStrings.redirectString}:</div>
-    <div class=${styles.tracking_result__text}>${
-      item.PossibilityCreateRedirecting ? UIStrings.yesString : UIStrings.noString
-    }<input type="button" class=${styles.request_action} disabled value="${
-      UIStrings.requestString
-    }"/></div>
-  </div>`;
-  });
-}
-
-window.getTrackingData = getTrackingData;
-window.addRequestSet = addRequestSet;
-window.removeRequestSet = removeRequestSet;
-window.resetApp = resetApp;
-
-function resetApp() {
+function resetForm(
+  documentsForRender = window.appState.documentsForRender,
+  requestedDocuments = window.appState.requestedDocuments,
+) {
   const requestSets = Array.from(document.querySelectorAll(`.${styles.request_set}`));
   requestSets.forEach((requestSet, index) => {
     if (index > 0) {
       requestSet.remove();
+    } else {
+      requestSet.querySelector('input[name = invoice_number]').value = '';
+      requestSet.querySelector('input[name = phone_number]').value = '';
+      requestSet.querySelector(`.${styles.tracking_result}`).innerHTML = '';
     }
   });
-  if (document.querySelector(`.${styles.tracking_result}`)) {
-    document.querySelector(`.${styles.tracking_result}`).innerHTML = '';
-  }
-  document.forms.request_form.reset();
+  documentsForRender.length = 0;
+  requestedDocuments.length = 0;
 }
 
-function renderMain() {
-  DOMFrameBlocks[1].innerHTML = `
-       <div class=${styles.tracking_form}>
-      <form name="request_form" autocomplete="off" onsubmit="getTrackingData(event)">
-        ${renderRequestSet()}
-        <input type="submit" value="${UIStrings.searchButtonString}" class=${
+function renderMain(
+  lang = window.appState.lang,
+  documentsForRender = window.appState.documentsForRender,
+) {
+  document.querySelector('main').innerHTML = `
+  <div class=${styles.tracking_form}>
+    <form name="request_form" autocomplete="off" onsubmit="getTrackingData(event)" oninput="updateFormState(event)">
+    ${renderRequestSets()}
+      <input type="submit" value="${UIStrings[lang].searchButtonString}" class=${
     styles.request_submit
   } />
-        <input type="button" value="${UIStrings.clearButtonString}" class=${
+      <input type="button" value="${UIStrings[lang].clearButtonString}" class=${
     styles.request_submit
-  } onclick="resetApp()" />
-      </form>
-    </div>`;
+  } onclick="resetForm()" />
+    </form>
+  </div>`;
+  if (documentsForRender.length != 0) {
+    renderResults();
+  }
 }
 
-function renderDynamicBlocks() {
-  renderHeader();
-  renderFooter();
-  renderMain();
+function updateFormState(event, requestedDocuments = window.appState.requestedDocuments) {
+  const requestSets = Array.from(document.querySelectorAll(`.${styles.request_set}`));
+  requestedDocuments.length = 0;
+  requestSets.forEach(prepareDocuments);
+
+  function prepareDocuments(requestItem) {
+    requestedDocuments.push({
+      DocumentNumber: requestItem.querySelector('input[name = invoice_number]').value,
+      Phone: requestItem.querySelector('input[name = phone_number]').value,
+    });
+  }
 }
 
-function renderRequestSet() {
+function renderRequestSets(requestedDocuments = window.appState.requestedDocuments) {
+  if (requestedDocuments.length == 0) {
+    return renderRequestSet();
+  } else {
+    return requestedDocuments
+      .map(item => {
+        return renderRequestSet(item.DocumentNumber, item.Phone);
+      })
+      .join('');
+  }
+}
+
+function renderRequestSet(invoiceNumber = '', phoneNumber = '', lang = window.appState.lang) {
   return `<div class=${styles.request_set}>
             <div class=${styles.request_item}>
-              <input type="text" name="invoice_number" value="" placeholder=" " required/>
-              <label for="invoice_number">${UIStrings.invoiceString}</label>
+              <input type="text" name="invoice_number" value="${invoiceNumber}" placeholder=" " required/>
+              <label for="invoice_number">${UIStrings[lang].invoiceString}</label>
             </div>
             <div class=${styles.request_item}>
-              <input type="text" name="phone_number" value="" pattern="[0-9]{0,10}" placeholder=" "/>
-              <label for="phone_number">${UIStrings.phoneString}</label>
+              <input type="text" name="phone_number" value="${phoneNumber}" pattern="[0-9]{0,10}" placeholder=" "/>
+              <label for="phone_number">${UIStrings[lang].phoneString}</label>
             </div>
             <input type="button" class=${styles.request_add_set} onclick="addRequestSet(event)" value="+"/>
             <input type="button" class=${styles.request_remove_set} onclick="removeRequestSet(event)" value="-"/>
@@ -177,15 +236,24 @@ function removeRequestSet() {
   event.target.parentNode.remove();
 }
 
+window.addRequestSet = addRequestSet;
+window.removeRequestSet = removeRequestSet;
+window.getTrackingData = getTrackingData;
+window.resetForm = resetForm;
+window.changeLanguage = changeLanguage;
+window.updateFormState = updateFormState;
+
+// preparing semantic HTML structure for app
 let topLevelFrameString = topLevelFrameBlocks.reduce(
   (renderString, item) => (renderString += renderSemanticBlock(item)),
   '',
 );
-
-function renderApp() {
-  document.body.insertAdjacentHTML('afterbegin', topLevelFrameString);
-  topLevelFrameBlocks.forEach(item => DOMFrameBlocks.push(document.querySelector(`${item}`)));
-  renderDynamicBlocks();
-}
-
+document.body.insertAdjacentHTML('afterbegin', topLevelFrameString);
 renderApp();
+
+function renderApp(lang = window.appState.lang) {
+  document.querySelector('HTML').setAttribute('lang', lang);
+  renderHeader();
+  renderMain();
+  renderFooter();
+}
--- variables.js
+++ variables.js
@@ -1,22 +1,42 @@
 export const topLevelFrameBlocks = ['header', 'main', 'footer'];
 
 export const UIStrings = {
-  invoiceString: 'Введите номер квитанции',
-  phoneString: 'Введите номер телефона',
-  searchButtonString: 'Искать',
-  clearButtonString: 'Очистить',
-  routeString: 'Маршрут',
-  weightString: 'Вес, кг',
-  placesString: 'Мест',
-  statusString: 'Текущий статус',
-  requestString: 'Заказать',
-  yesString: 'да',
-  noString: 'нет',
-  returnString: 'Можно вернуть',
-  redirectString: 'Можно переслать',
-  headerString: 'Трекинг посылок Новой почты',
-  footerString: 'Версия 1.3',
-  resultString: 'Результат',
+  ru: {
+    invoiceString: 'Введите номер квитанции',
+    phoneString: 'Введите номер телефона',
+    searchButtonString: 'Искать',
+    clearButtonString: 'Очистить',
+    routeString: 'Маршрут',
+    weightString: 'Вес, кг',
+    placesString: 'Мест',
+    statusString: 'Текущий статус',
+    requestString: 'Заказать',
+    yesString: 'да',
+    noString: 'нет',
+    returnString: 'Можно вернуть',
+    redirectString: 'Можно переслать',
+    headerString: 'Трекинг посылок Новой почты',
+    footerString: 'Версия',
+    resultString: 'Результат',
+  },
+  ua: {
+    invoiceString: 'Введіть номер квитанції',
+    phoneString: 'Введіть номер телефону',
+    searchButtonString: 'Шукати',
+    clearButtonString: 'Очистити',
+    routeString: 'Маршрут',
+    weightString: 'Вага, кг',
+    placesString: 'Місць',
+    statusString: 'Поточний статус',
+    requestString: 'Замовити',
+    yesString: 'так',
+    noString: 'ні',
+    returnString: 'Можна повернути',
+    redirectString: 'Можна переслати',
+    headerString: 'Трекінг посилок Нової пошти',
+    footerString: 'Версія',
+    resultString: 'Результат',
+  },
 };
 
 export const url = 'https://api.novaposhta.ua/v2.0/json/';
@@ -31,3 +51,4 @@ export const requestOptions = {
 };
 
 export const freshnessTime = 18000000;
+export const appVersion = '1.4';
